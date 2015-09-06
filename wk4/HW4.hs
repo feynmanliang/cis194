53ebcb91f6727f68e16ad788daca3ae232fcb55b
Feynman Liang <fliang@databricks.com>
2015-09-05 21:52:33 -0700

    Finishes wk4

--- wk4/HW4.hs
+++ wk4/HW4.hs
@@ -2,6 +2,9 @@
 
 module HW4 where
 
+import Data.List
+import Test.HUnit
+
 {-Q1-}
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
@@ -56,10 +59,28 @@ map' :: (a -> b) -> [a] -> [b]
 map' f = foldr (\x xs -> f(x) : xs) []
 
 myFoldl :: (a -> b -> a) -> a -> [b] -> a
-myFoldl f base xs = case xs of
-  [] -> base
-  (y:ys) -> foldr (\a g a' -> f (g a') a) (f base) ys y
+myFoldl f base xs = foldr (\x acc y -> acc (y `f` x)) id xs base
 
 -- foldr f z [x1, x2, ..., xn] == x1 ‘f‘ (x2 ‘f‘ ... (xn ‘f‘ z)...)
 -- foldl f z [x1, x2, ..., xn] == (...((z ‘f‘ x1) ‘f‘ x2) ‘f‘...) ‘f‘ xn
 
+{-Q4: Sieve of Sundaram -}
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map (\x -> 2 * x + 1) . (`sub` ijs) $ [1..n]
+    where
+        ijs = takeWhile (<= n) . map head . group . sort $ [i + j + 2*i*j | j <- [1..n], i <- [1.. (n - j) `div` (1 + 2*j)]]
+
+        sub [] _  = []
+        sub xs [] = xs
+        sub xxs@(x:xs) yys@(y:ys)
+          | x < y     = x : sub xs yys
+          | x == y    = sub xs ys
+          | otherwise = sub xxs ys
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+cl :: IO Counts
+cl = runTestTT (TestList [
+  cartProd [1,2] ['a','b'] ~?= [(1,'a'),(1,'b'),(2,'a'),(2,'b')]
+  ])
